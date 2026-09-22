5151e6306fd08afb879f26d417caa409fea395e5
Abidur Rahim <42abid@gmail.com>
2026-09-22 17:34:17 -0400

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AlarmsView from '../views/AlarmsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,6 +8,11 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
+      component: AlarmsView,
+    },
+    {
+      path: '/home',
+      name: 'old home',
       component: HomeView,
     },
     {
