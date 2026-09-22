0db0fafd59453a571c9c7ab65714d00fe2cadd9d
Abidur Rahim <42abid@gmail.com>
2026-09-22 17:34:14 -0400

    Create AlarmsView.vue

---
+++ src/views/AlarmsView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="wrapper">
+    <h1>Alarms</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
