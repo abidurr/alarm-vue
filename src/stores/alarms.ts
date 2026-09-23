c4bc8b9fbf0a17831ad6e297412fc1a6ad64b129
Abidur Rahim <42abid@gmail.com>
2026-09-22 20:06:16 -0400

    alarm store

---
+++ src/stores/alarms.ts
@@ -0,0 +1,31 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+import moment from 'moment'
+
+interface IAlarm {
+  order: number
+  id: string
+  time: string
+  label: string
+}
+
+export const useAlarmsStore = defineStore('alarms', () => {
+  const alarms = ref([
+    { order: 0, id: 'shagudhaskjdkajs', time: moment().format('HH:mm'), label: 'My Label 1' },
+  ] as IAlarm[])
+  const length = computed(() => alarms.value.length)
+
+  function addAlarm(alarm: IAlarm) {
+    alarms.value = [...alarms.value, alarm]
+  }
+
+  function removeAlarm(id: string) {
+    alarms.value = alarms.value.filter((alarm) => alarm.id !== id)
+  }
+
+  function updateAlarm(id: string, alarm: IAlarm) {
+    alarms.value = alarms.value.map((a) => (a.id === id ? alarm : a))
+  }
+
+  return { alarms, length, addAlarm, removeAlarm, updateAlarm }
+})
