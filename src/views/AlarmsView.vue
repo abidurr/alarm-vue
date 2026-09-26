17134ec38ce05e8c9eb56308402ff3b7e2c3c4ac
Abidur Rahim <42abid@gmail.com>
2026-09-26 16:57:19 -0400

    loading bar for alarm progress

--- src/views/AlarmsView.vue
+++ src/views/AlarmsView.vue
@@ -10,6 +10,13 @@ import moment from 'moment-timezone'
 const alarms = useAlarmsStore()
 const time_input = ref(moment().format('HH:mm:ss'))
 const label = ref(`Alarm ${alarms.alarms.length + 1}`)
+
+const SECONDS_PER_DAY = 24 * 3600
+
+const timeToAlarm = (time: string): number => {
+  const timeToAlarm = moment(time, 'HH:mm:ss').diff(moment(), 'seconds')
+  return (SECONDS_PER_DAY - timeToAlarm) / SECONDS_PER_DAY
+}
 </script>
 
 <template>
@@ -57,17 +64,19 @@ const label = ref(`Alarm ${alarms.alarms.length + 1}`)
       </button>
     </div>
     <div class="single-alarm" v-for="alarm in alarms.alarms" :key="alarm.id">
+      <div class="progress-bar" :style="{ width: `${timeToAlarm(alarm.time) * 100}%` }"></div>
       <span class="mono-font" style="font-weight: 600; margin-top: 4px">{{ alarm.order }}.</span>
-      <div>
-        <span style="font-style: italic; align-content: baseline">{{ alarm.label }}</span> at
+      <div style="z-index: 2">
+        <span style="font-style: italic; align-content: baseline">{{ alarm.label }}</span
+        >&nbsp; at
         <span class="mono-font" style="font-weight: 600">{{ alarm.time }}</span>
       </div>
       <button
-        style="margin-left: 16px; border-radius: 50%"
+        style="margin-left: 16px; border-radius: 50%; z-index: 2"
         class="mono-font close-button"
         @click="() => alarms.removeAlarm(alarm.id)"
       >
-        X
+        OK
       </button>
     </div>
   </div>
@@ -77,22 +86,40 @@ const label = ref(`Alarm ${alarms.alarms.length + 1}`)
 .inputdiv {
   display: flex;
   align-items: center;
+  justify-content: center;
   gap: 12px;
   padding: 40px 0px;
   margin: 0px;
 }
+
+.progress-bar {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  transition: width 0.3s ease-in-out;
+  /*width: 27%;*/
+  background-color: rgba(50, 50, 200, 0.6);
+  border-radius: 30px;
+  z-index: 1;
+  max-width: 100%;
+}
 .single-alarm {
+  position: relative;
   display: flex;
   justify-content: space-between;
   background: black;
-  border-radius: 24px;
-  margin: 12px 0px;
+  border-radius: 26px;
+  margin: 16px 0px;
   padding: 12px 36px;
   text-align: center;
   color: ghostwhite;
   box-shadow:
     0 4px 6px rgba(50, 50, 200, 0.7),
     0 5px 15px rgba(5, 5, 20, 0.1);
+  span {
+    z-index: 2;
+  }
 }
 
 @media (min-width: 1024px) {
